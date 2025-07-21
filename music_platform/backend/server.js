7ccf9f3845f72b5f279225e1d9b34be80f577f07
rscbpch <bopich568317@gmail.com>
2025-07-21 16:21:26 +0700

    remove duplicate router

--- music_platform/backend/server.js
+++ music_platform/backend/server.js
@@ -7,7 +7,6 @@ const userRouter = require("./routes/user.js");
 
 
 const playlistRouter = require("./routes/playlist.js");
-const songRouter = require("./routes/song.js");
 const albumRouter = require("./routes/album.js");
 const playHistoryRouter = require("./routes/playhistory.js");
 
@@ -16,12 +15,6 @@ const app = express();  // <-- initialize app first
 
 const songRouter = require("./routes/song.js");
 const artistRouter = require("./routes/artist.js");
-const albumRouter = require("./routes/album.js");
-
-
-const songRouter = require("./routes/song.js");
-const artistRouter = require("./routes/artist.js");
-const albumRouter = require("./routes/album.js");
 app.use(express.json());
 
 app.use(cors());
@@ -31,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Now all app.use routes:
 app.use('/api/users', userRouter);
 app.use("/api/playlists", playlistRouter);
-app.use("/api/songs", songRouter);
 app.use("/api/albums", albumRouter);
 app.use("/api/playhistory", playHistoryRouter);
 
