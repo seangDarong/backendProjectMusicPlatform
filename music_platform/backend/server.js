ea411e242b1eea0daffcf8af9d95e4d3fafa33d3
Phon Sokleaphea <phonsokleaphea@Phons-MacBook-Pro.local>
2025-07-21 16:23:15 +0700

    comit to pull

--- music_platform/backend/server.js
+++ music_platform/backend/server.js
@@ -4,24 +4,15 @@ const cors = require('cors');
 const { sequelize } = require('./models');
 
 const userRouter = require("./routes/user.js");
-
-
 const playlistRouter = require("./routes/playlist.js");
 const songRouter = require("./routes/song.js");
 const albumRouter = require("./routes/album.js");
 const playHistoryRouter = require("./routes/playhistory.js");
+const artistRouter = require("./routes/artist.js");
+
 
 const app = express();  // <-- initialize app first
 
-
-const songRouter = require("./routes/song.js");
-const artistRouter = require("./routes/artist.js");
-const albumRouter = require("./routes/album.js");
-
-
-const songRouter = require("./routes/song.js");
-const artistRouter = require("./routes/artist.js");
-const albumRouter = require("./routes/album.js");
 app.use(express.json());
 
 app.use(cors());
@@ -34,14 +25,9 @@ app.use("/api/playlists", playlistRouter);
 app.use("/api/songs", songRouter);
 app.use("/api/albums", albumRouter);
 app.use("/api/playhistory", playHistoryRouter);
-
+app.use('/api/artists', artistRouter);
 const port = 3000;
 
-
-app.use('/api/users', userRouter);
-app.use('/api/songs', songRouter);
-app.use('/api/artists', artistRouter);
-app.use('/api/albums', albumRouter);
 
 sequelize.sync({ alter: true }).then(() => {
     console.log('Database synced successfully.');
