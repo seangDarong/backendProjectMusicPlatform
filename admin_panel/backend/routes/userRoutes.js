b6a1da421f12dde4aca0cb9a44dfdb2fd1d7f336
pich <bopich568317@gmail.com>
2025-07-28 02:44:57 +0700

    plech . muy

--- admin_panel/backend/routes/userRoutes.js
+++ admin_panel/backend/routes/userRoutes.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/userController');
 router.get('/', userController.getAllUser);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.createUser);
-router.put('/:id', userControllerupdateUser);
+router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
