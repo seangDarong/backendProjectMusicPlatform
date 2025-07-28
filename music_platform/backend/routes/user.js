90b4451e4aae6023f15aa9bc487e4b1ae1309c7f
rong <darong6126@gmail.com>
2025-07-28 16:32:21 +0700

    add user diactivate acc and forgot password but not yet work

--- music_platform/backend/routes/user.js
+++ music_platform/backend/routes/user.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const authMiddleware = require('../middleware/auth.js');
 const UserController = require('../controllers/user.js');
+const inputMiddleWare = require('../middleware/inputValidation.js');
 
 const userRouter = express.Router();
 
-userRouter.post('/register', UserController.register);
+userRouter.post('/register',inputMiddleWare, UserController.register);
 userRouter.post('/login', UserController.login);
+userRouter.post('/reset-password', UserController.resetPassword);
+userRouter.delete('/deactivate', authMiddleware, UserController.deactivateAccount);
+
 
 userRouter.get('/profile', authMiddleware, UserController.getProfile);
 userRouter.put('/profile', authMiddleware, UserController.updateProfile);
